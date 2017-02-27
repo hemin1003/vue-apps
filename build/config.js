8d5054091a7f39b1d5c1f7bb2ead00c7b77a450d
贺敏 <hemin_it@163.com>
2017-02-27 16:50:58 +0800

    update

--- build/config.js
+++ build/config.js
@@ -3,7 +3,7 @@ var path = require('path')
 module.exports = {
   build: {
     // index: path.resolve('dist/page/index.html'),
-    assetsRoot: path.resolve('../yz-wx'), // path.resolve('dist'), // d:\leo-work\vue-pages\dist
+    assetsRoot: path.resolve('../yz-wx-tmp'), // path.resolve('dist'), // d:\leo-work\vue-pages\dist
     assetsSubDirectory: 'static',
     // assetsPublicPath: process.env.NODE_ENV === 'production' ? '../' : '/',
     assetsPublicPath: '/',
